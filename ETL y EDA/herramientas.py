0f8e9fab4d58e390fd94ac5261a76c784282292e
AgustinBalcaza <121921577+AgustinBalcaza@users.noreply.github.com>
2023-11-28 10:18:10 -0300

    add ETL/EDA

---
+++ ETL y EDA/herramientas.py
@@ -0,0 +1,38 @@
+import pandas as pd
+
+def reemplazar_nulos(df):
+
+    for column in df.columns:
+        mask = df[column].notnull()
+        dtype = df[column][mask].apply(type).unique()
+
+        if dtype[0] == str:
+            df[column] = df[column].fillna('No data')
+        if dtype[0] == float:
+            mean = df[column].mean()
+            df[column] = df[column].fillna(mean)
+
+
+def data_info(df):
+
+    mi_dict = {"Column": [], "dType": [], "No_Null_%": [], "No_Null_Qty": [], "Null_%": [], "Null_Qty": []}
+    duplicated_rows = df[df.duplicated()]
+    count_duplicated_rows = len(duplicated_rows)
+
+
+    for columna in df.columns:
+        porcentaje_no_nulos = (df[columna].count() / len(df)) * 100
+        mi_dict["Column"].append(columna)
+        mi_dict["dType"].append(df[columna].apply(type).unique())
+        mi_dict["No_Null_%"].append(round(porcentaje_no_nulos, 2))
+        mi_dict["No_Null_Qty"].append(df[columna].count())
+        mi_dict["Null_%"].append(round(100-porcentaje_no_nulos, 2))
+        mi_dict['Null_Qty'].append(df[columna].isnull().sum())
+
+    df_info = pd.DataFrame(mi_dict)
+    
+    print("\nTotal rows: ", len(df))
+    print("\nTotal full null rows: ", df.isna().all(axis=1).sum())
+    print("\nTotal duplicated rows:", count_duplicated_rows)
+    
+    return df_info
